5f4709f7c7360029ac75b760e96e2f6c13cf746c
misakikondo <misakikondo718@gmail.com>
2020-09-18 20:33:42 +0900

    バリデーションの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,25 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+validates :email, uniqueness: true,
+                  format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
+
+validates :password, format: {with: /\A[a-zA-Z0-9]+\z/}
+  
+validates :nickname, presence: true
+
+with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
+  validates :family_name
+  validates :first_name
+end
+
+with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/ } do
+  validates :family_name_kana
+  validates :first_name_kana
+end
+
+validates :birth, presence: true
+
+
 end
