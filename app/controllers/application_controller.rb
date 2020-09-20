4b7dc462ffb5843a5c7049988c5726770aba227e
misakikondo <misakikondo718@gmail.com>
2020-09-20 15:14:24 +0900

    Rubocop自動修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,15 +6,11 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["SECRET_USER"] && password == ENV["SECRET_KEY"]
+      username == ENV['SECRET_USER'] && password == ENV['SECRET_KEY']
     end
   end
-
-
-  private
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birth])
   end
-  
 end
