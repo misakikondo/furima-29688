4d9bee014e2b1047a8e9c828aac4fd4826ae25b4
misakikondo <misakikondo718@gmail.com>
2020-09-20 15:12:55 +0900

    モデル単体テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,24 +4,23 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-validates :email, uniqueness: true,
-                  format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
+  validates :email, uniqueness: true,
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
 
-validates :password, format: {with: /\A[a-zA-Z0-9]+\z/}
-  
-validates :nickname, presence: true
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i },
+                       length: { minimum: 6 }
 
-with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
-  validates :family_name
-  validates :first_name
-end
+  validates :nickname, presence: true
 
-with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/ } do
-  validates :family_name_kana
-  validates :first_name_kana
-end
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
+    validates :family_name
+    validates :first_name
+  end
 
-validates :birth, presence: true
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/ } do
+    validates :family_name_kana
+    validates :first_name_kana
+  end
 
-
+  validates :birth, presence: true
 end
