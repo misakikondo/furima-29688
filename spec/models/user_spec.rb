b0d47a427c6b10c9bc1b53986f23075a2836f30f
misakikondo <misakikondo718@gmail.com>
2020-09-20 12:16:21 +0900

    単体モデルコードの準備

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+RSpec.describe User, type: :model do
+  describe 'ユーザー新規登録' do
+    it 'nicknameが空だとできない' do
+    end
+
+    it 'emailが空だとできない' do
+    end
+
+    it 'emailが登録済みだとできない' do
+    end
+
+    it 'passwordが空だとできない'do
+    end
+    
+    it do
+    end
+    it do
+    end
+    it do
+    end
+    it do
+    end
+    it do
+    end
+  end
+
+
+end
