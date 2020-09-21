05d8f024526da2e31eefc477d7a2ec254078af84
misakikondo <misakikondo718@gmail.com>
2020-09-21 14:35:36 +0900

    その他Activehashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,14 +4,18 @@ class Item < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :shipping_method
+  belongs_to_active_hash :item_location
+  belongs_to_active_hash :days_to_ship
 
 
   with_options presence: true do
-    validates :category
+    validates :items_name,:explains,:category,:condition,:shipping_method,:item_location,:days_to_ship
   end
 
-  with_options presence: true do
-    validates :category_id, numerically: { other_than:1 }
+  with_options numerically: { other_than:1 } do
+    validates :category_id,:condition_id,:shipping_method_id,:item_location_id,:days_to_ship_id
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -33,6 +33,21 @@ ActiveRecord::Schema.define(version: 2020_09_21_020706) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "items_name", null: false
+    t.text "explains", null: false
+    t.integer "category_id", null: false
+    t.integer "condition_id", null: false
+    t.integer "shipping_method_id", null: false
+    t.integer "item_location_id", null: false
+    t.integer "days_to_ship_id", null: false
+    t.integer "price", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -52,4 +67,5 @@ ActiveRecord::Schema.define(version: 2020_09_21_020706) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "items", "users"
 end
