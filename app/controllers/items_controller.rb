bb3d979ea60cade727fc96ed90c2341b01276ef8
misakikondo <misakikondo718@gmail.com>
2020-09-21 11:46:06 +0900

    ActiveStorageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,16 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: :new
+
   def index
   end
+
+  def new
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image).merge(user_id: current_user.id)
+  end
+
 end
