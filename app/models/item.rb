39050b0b2ef0cb634f09d31d5efed360751efe36
misakikondo <misakikondo718@gmail.com>
2020-09-21 19:39:38 +0900

    Itemモデルのバリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,9 +2,6 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :items_name, presence: true, length: { maximum: 40 }
-  validates :explains, presence: true, length: { maximum: 1000 }
-
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
   belongs_to_active_hash :condition
@@ -12,15 +9,22 @@ class Item < ApplicationRecord
   belongs_to_active_hash :item_location
   belongs_to_active_hash :days_to_ship
 
-
-  with_options presence: true do
+  validates :image, presence: true
+  validates :items_name, presence: true, length: { maximum: 40 }
+  validates :explains, presence: true, length: { maximum: 1000 }
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message:"Out of setting range" },
+             format: { with: /\A[0-9]+\z/,  message:"Half-width number"}
+             
+             
+    with_options presence: true do
     validates :category, :condition, :shipping_method, 
               :item_location, :days_to_ship
-  end
+    end
 
-  with_options numerically: { other_than:1 } do
-    validates :category_id, :condition_id, :shipping_method_id, 
-              :item_location_id, :days_to_ship_id
-  end
+  validates :category_id, numericality: { other_than: 1, message: "Select" } 
+  validates :condition_id, numericality: { other_than: 1, message: "Select" } 
+  validates :shipping_method_id, numericality: { other_than: 1, message: "Select" } 
+  validates :item_location_id, numericality: { other_than: 1, message: "Select" } 
+  validates :days_to_ship_id, numericality: { other_than: 1, message: "Select" }
 
 end
