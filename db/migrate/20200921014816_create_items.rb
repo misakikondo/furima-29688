7a3b8ed92cbb3c87c68dde4fafce3c0e1ad10e08
misakikondo <misakikondo718@gmail.com>
2020-09-21 11:46:30 +0900

    itemsテーブルにカラムを追加

--- db/migrate/20200921014816_create_items.rb
+++ db/migrate/20200921014816_create_items.rb
@@ -1,6 +1,10 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
+      t.string         :items_name,  null: false
+      t.text           :explains,    null: false
+      t.integer        :price,       null: false
+      t.references     :user,        null: false,foreign_key: true
       t.timestamps
     end
   end
