e28620cac7d1360f339d9776fdc193fef6414672
misakikondo <misakikondo718@gmail.com>
2020-09-21 12:43:06 +0900

    Categoryの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,16 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+
+
+  with_options presence: true do
+    validates :category
+  end
+
+  with_options presence: true do
+    validates :category_id, numerically: { other_than:1 }
+  end
+
 end
--- db/migrate/20200921014816_create_items.rb
+++ db/migrate/20200921014816_create_items.rb
@@ -1,10 +1,11 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-      t.string         :items_name,  null: false
-      t.text           :explains,    null: false
-      t.integer        :price,       null: false
-      t.references     :user,        null: false,foreign_key: true
+      t.string         :items_name,     null: false
+      t.text           :explains,       null: false
+      t.integer        :category_id,    null: false  
+      t.integer        :price,          null: false
+      t.references     :user,           null: false, foreign_key: true
       t.timestamps
     end
   end
