822a4b230d6d5185ba1676904e88d2e8e8afb0d5
misakikondo <misakikondo718@gmail.com>
2020-09-22 15:27:11 +0900

    金額計算（非同期）の実装

---
+++ app/javascript/charge.js
@@ -0,0 +1,13 @@
+function charge (){
+  const itemPrice = document.getElementById("item-price");
+  const addTaxPrice = document.getElementById("add-tax-price");
+  const Profit = document.getElementById("profit");
+  itemPrice.addEventListener('keyup', () => {
+    const Value = itemPrice.value;
+    const chargeTax = Math.floor(Value * 0.1);
+    const chargeProfit = Math.floor(Value * 0.9);
+    addTaxPrice.innerHTML = `${chargeTax.toLocaleString()}`;
+    Profit.innerHTML = `${chargeProfit.toLocaleString()}`;
+  });
+}
+window.addEventListener('load', charge);
