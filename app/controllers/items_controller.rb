13f4e03a4c252b7bea99b4906a7b8a9412c5eb61
misakikondo <misakikondo718@gmail.com>
2020-09-22 19:17:23 +0900

    Rubocop自動修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: :new
 
   def index
-    @items = Item.order("created_at DESC")
+    @items = Item.order('created_at DESC')
   end
 
   def new
@@ -25,7 +25,6 @@ class ItemsController < ApplicationController
       :image, :items_name, :explains,
       :category_id, :condition_id, :shipping_method_id,
       :item_location_id, :days_to_ship_id, :price
-      ).merge(user_id: current_user.id)
+    ).merge(user_id: current_user.id)
   end
-
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -12,19 +12,17 @@ class Item < ApplicationRecord
   validates :image, presence: true
   validates :items_name, presence: true, length: { maximum: 40 }
   validates :explains, presence: true, length: { maximum: 1000 }
-  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message:"Out of setting range" },
-             format: { with: /\A[0-9]+\z/,  message:"Half-width number"}
-             
-             
-    with_options presence: true do
-    validates :category, :condition, :shipping_method, 
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'Out of setting range' },
+                    format: { with: /\A[0-9]+\z/, message: 'Half-width number' }
+
+  with_options presence: true do
+    validates :category, :condition, :shipping_method,
               :item_location, :days_to_ship
-    end
+  end
 
-  validates :category_id, numericality: { other_than: 1, message: "Select" } 
-  validates :condition_id, numericality: { other_than: 1, message: "Select" } 
-  validates :shipping_method_id, numericality: { other_than: 1, message: "Select" } 
-  validates :item_location_id, numericality: { other_than: 1, message: "Select" } 
-  validates :days_to_ship_id, numericality: { other_than: 1, message: "Select" }
-
+  validates :category_id, numericality: { other_than: 1, message: 'Select' }
+  validates :condition_id, numericality: { other_than: 1, message: 'Select' }
+  validates :shipping_method_id, numericality: { other_than: 1, message: 'Select' }
+  validates :item_location_id, numericality: { other_than: 1, message: 'Select' }
+  validates :days_to_ship_id, numericality: { other_than: 1, message: 'Select' }
 end
