9ac91e5e0d3b35fe6a470028569898e9b930c988
misakikondo <misakikondo718@gmail.com>
2020-09-25 21:16:16 +0900

    コントローラーの修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,23 @@
 class OrdersController < ApplicationController
 
   def new 
+    @order = OrderAddress.new
   end
 
   def create
+    @order = OrderAddress.new(order_params)
+    if @order.valid?
+      @order.save
+      return redirect_to root_path
+    else
+      render :new
+    end
   end
-  
+
+
+  private
+  def order_params
+    params.require(:order_address).permit(:user_id,:item_id,:zip_code,:prefecture,:city,:block,:bulimg_name,:phone,:order_id)
+  end
+
 end
