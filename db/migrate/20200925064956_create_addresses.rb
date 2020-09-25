64f11f88436ad1169a85dbcdc5333c61b76422e9
misakikondo <misakikondo718@gmail.com>
2020-09-25 21:15:09 +0900

    addressesテーブルにカラムを追加

--- db/migrate/20200925064956_create_addresses.rb
+++ db/migrate/20200925064956_create_addresses.rb
@@ -1,7 +1,13 @@
 class CreateAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :addresses do |t|
-
+      t.string     :zip_code,       null: false
+      t.integer    :prefecture,     null: false
+      t.string     :city,           null: false
+      t.string     :block,          null: false
+      t.string     :building_name,  null: false
+      t.string     :phone,          null: false
+      t.references  :order,  null: false, foreign_key: true
       t.timestamps
     end
   end
