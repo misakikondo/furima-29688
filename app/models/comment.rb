5d8f936ce96f7cee04af79593e6ffd7af885afbc
misakikondo <misakikondo718@gmail.com>
2020-10-01 11:47:22 +0900

    Commentモデルの修正
    バリデーションの追加
    絵文字を入力できないようにした

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,14 @@ class Comment < ApplicationRecord
  belongs_to :item, dependent: :destroy
 
  validates :text, presence: true
+ validate :no_4_bytes
+
+def no_4_bytes
+  if text.present?
+    chars = text.each_char.select{|c| c.bytes.count >= 4}
+    if chars.size > 0
+        errors.add(:text, "に絵文字(#{chars.join('')})は使用できません。")
+    end
+  end
+end
 end
